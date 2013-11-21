986329f163cf36682d353a52a9e21bc68c9cc75e
Ryan Harris <rmharris713@gmail.com>
2013-11-21 10:02:15 -0600

    first commit

---
+++ QuickScraper.user.js
@@ -0,0 +1,14 @@
+// ==UserScript==
+// @name        Quick Scraper
+// @namespace   https://github.com/rmharris713/QuickScraper.git
+// @description Tool for personal lead gen
+// @version     1.0
+// @date        2013-11-21
+// @creator     rharris@yodle.com
+// @include     *
+// @grant       GM_getValue
+// @grant       GM_setValue
+// @grant       GM_log
+// @grant       GM_xmlhttpRequest
+// @run-at      document-end
+// ==/UserScript==
